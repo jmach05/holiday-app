3bdebce2b83588f7f0573f5b2a8dc78d8aa3e281
jmach05 <jibumon14@gmail.com>
2022-12-21 19:53:53 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/services" element={<Services />}></Route>
+          <Route path="/products" element={<Products />}></Route>
+          <Route path="/sign-up" element={<SignUp />}></Route>
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+
+  )
+}
+
+export default App;
